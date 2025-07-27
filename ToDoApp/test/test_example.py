96187bbcfa790e88ca579a7063d0f9578a69b105
Rahul Sharma <ravigeneralstore161@gmail.com>
2025-07-27 11:33:32 +0530

    updated notes

--- ToDoApp/test/test_example.py
+++ ToDoApp/test/test_example.py
@@ -29,5 +29,19 @@ def test_list():
     assert not any(any_list)
 
 
+class Student:
+    def __init__(self, first_name: str, last_name: str, major: str, years: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+def test_person_initialization():
+    p = Student("John", "Doe", "Computer Science", 3)
+    assert p.first_name == "John", 'First Name should be John'
+    assert p.last_name == "Doe", 'LastName should be Doe'
+    assert p.major == "Computer Science", 'Major should be Computer Science'
+    assert p.years == 3
+
 
 
